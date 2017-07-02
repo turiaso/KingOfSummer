5d56b482e6aa628d35682418842adc2c384d6897
Alien-PC\Alien <juan.ruizpachon@gmail.com>
2017-07-02 11:41:28 +0200

    no message

--- Assets/scripts/CharacterSceneButtonScript.js
+++ Assets/scripts/CharacterSceneButtonScript.js
@@ -1,20 +1,22 @@
 ﻿#pragma strict
-var ls:UnityEngine.SceneManagement.SceneManager;
 private var audioLoaded:AudioSource; 
 function Start () {
 	audioLoaded = GetComponent.<AudioSource>();
 }
 
 function Update () {
- if (Input.GetMouseButton(0)){
-	 audioLoaded.Play();
-	 audioLoaded.Play(44100);
-		loadScene();
-	}
 	
 }
+
 function loadScene(){
 	yield WaitForSeconds (1);
-	ls.LoadScene("StageScene");
+	UnityEngine.SceneManagement.SceneManager.LoadScene("StageScene");
 	
 }
+
+function OnMouseDown () {
+	audioLoaded.Play();
+	audioLoaded.Play(44100);
+	loadScene();
+}
+
