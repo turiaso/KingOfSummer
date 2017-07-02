afbfd5f15a2eadf60ba55f183f75a229989b7251
Alien-PC\Alien <juan.ruizpachon@gmail.com>
2017-07-02 11:35:31 +0200

    no message

--- Assets/scripts/Player.js
+++ Assets/scripts/Player.js
@@ -4,7 +4,8 @@ public var credits:int;
 public var score:int;
 
 function Start () {
-	
+	var data =  GameObject.Find("GameData").GetComponent(CommunicationAmongScenesScript).getData();
+	this.credits = data["credits"];
 }
 
 function Update () {
