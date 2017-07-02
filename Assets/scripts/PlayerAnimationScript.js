30a03ec106d7f1cd5a31a25da17ee925b955d8b9
DESKTOP-8D6E4L6\reverendo <rvdo.santiago@gmail.com>
2017-07-02 15:57:15 +0200

    no message

---
+++ Assets/scripts/PlayerAnimationScript.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+public var sprites:Sprite[];
+private var counterTime = 0;
+private var timeToChange = 333;
+private var status =0;
+
+function Start () {
+	
+}
+
+function Update () {
+	counterTime+=Time.deltaTime*1000;
+	if(counterTime>timeToChange){
+		//cambiamos sprite
+		counterTime = counterTime-timeToChange;
+		changeSprite(status);
+		status =(status+1)% sprites.length;
+	}
+}
+
+function changeSprite(status){
+		 GetComponent(SpriteRenderer).sprite = sprites[status];
+}
+
+function getStatus(){
+	return status;
+}
