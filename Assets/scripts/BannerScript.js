c8e5121a70bec031f21060e31c2a1f97c71d795c
Alien-PC\Alien <juan.ruizpachon@gmail.com>
2017-07-02 03:41:10 +0200

    no message

---
+++ Assets/scripts/BannerScript.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var ls:UnityEngine.SceneManagement.SceneManager;
+private var rb: Rigidbody;
+
+function Start () {
+	rb = GetComponent.<Rigidbody>();
+}
+
+function Update () {	
+	if(Input.GetMouseButtonDown(0)){
+		Debug.Log("OnMouseDown2 ");
+		rb.AddForce(0,15,0,ForceMode.Impulse);
+	}
+	if(transform.position.y<-5){
+		ls.LoadScene("MainScene");
+	}
+}
+function OnMouseDown () {  
+	Debug.Log("OnMouseDown ");
+	rb.AddForce(0,15,0,ForceMode.Impulse);
+}
